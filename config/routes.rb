6435bbdd4b9f419c8826de84e811527dee0b8e31
mizukenw <kentamizu75@gmail.com>
2021-07-02 21:47:46 +0900

    いいね機能追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 Rails.application.routes.draw do
-  devise_for :users, # ここの行にカンマを追加
-    controllers: { registrations: 'registrations' } # ここの行を追加
+  devise_for :users, 
+    controllers: { registrations: 'registrations' } 
 
-  root 'pages#home'
+  root 'posts#index'
 
   get '/users/:id', to: 'users#show', as: 'user'
 
-  resources :posts, only: %i(new create) do
+  resources :posts, only: %i(new create index show destroy) do
     resources :photos, only: %i(create)
+    resources :likes, only: %i(create destroy)
   end 
 end
