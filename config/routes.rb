9c2b23a9e9e8a86bd1aa59accca71eda3c2052b1
mizukenw <kentamizu75@gmail.com>
2021-07-02 15:25:11 +0900

    投稿機能実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
-  devise_for :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  devise_for :users, # ここの行にカンマを追加
+    controllers: { registrations: 'registrations' } # ここの行を追加
+
   root 'pages#home'
+
   get '/users/:id', to: 'users#show', as: 'user'
+
+  resources :posts, only: %i(new create) do
+    resources :photos, only: %i(create)
+  end 
 end
